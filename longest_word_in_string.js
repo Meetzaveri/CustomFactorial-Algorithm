df56db99a0c8e0c91885ca1559d02584cfcfb12a
Meet Zaveri <meethpatel96@gmail.com>
2017-08-25 23:53:44 +0530

    Create longest_word_in_string.js

---
+++ longest_word_in_string.js
@@ -0,0 +1,18 @@
+function findLongestWord(str) {
+  // First We Split it into word as allowing one space as ' ' in split 
+  //  argument as it will contain array of words
+  var spl=str.split(' ') ;
+  var foo=[];
+  for(var i=0;i<spl.length;i++){
+    foo.push(spl[i].length); 
+  }
+  //Using Spread Operator to calculate max. no. from array
+  var max = Math.max(...foo);
+  // Or We can use arr.reduce function to compare and return max from an array
+  return max;
+}
+
+findLongestWord("The quick brown fox jumped over the lazy dog");
+
+
+//Note : Here there is ESlint(ECMAScript 2015) operator.
