0537cb3ae18c0b775781cec684a763a822299f2f
Meet Zaveri <meethpatel96@gmail.com>
2017-08-27 23:42:59 +0530

    Create return_largest_numbers_in_array.js

---
+++ return_largest_numbers_in_array.js
@@ -0,0 +1,19 @@
+
+
+
+function largestOfFour(arr) {
+  // You can do this!
+  var largestnumbers=[0,0,0,0];
+  for (var i = 0;i<arr.length;i++){                 //Outer Loop Iteration
+    for(var k = 0;k <arr[i].length;k++){            //Inner Loop Iteration
+      if(arr[i][k] > largestnumbers[i]){
+      largestnumbers[i] = arr[i][k];                // Result After Comparision
+        }
+      console.log(largestnumbers[i]);
+    }
+  }
+  console.log('After for loop');
+  return largestnumbers;
+}
+
+largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]);
