0008e1342d5ddf60fca61ed9e33b580b2d952523
Meet Zaveri <meethpatel96@gmail.com>
2017-11-28 22:37:10 +0530

    Create chunky_monkey.js

---
+++ chunky_monkey.js
@@ -0,0 +1,13 @@
+function chunkArrayInGroups(arr, size) {
+  // Break it up.
+  var group = [];
+  var position = 0;
+  
+  while(position < arr.length) {
+    group.push(arr.slice(position, position+=size));
+  }
+  return group;
+}
+
+
+chunkArrayInGroups(["a", "b", "c", "d"], 2);
