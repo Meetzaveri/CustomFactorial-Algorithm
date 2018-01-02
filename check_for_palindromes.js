4ca794948af0bb3ac1e96982bb7d1049d0bdcf71
Meet Zaveri <meethpatel96@gmail.com>
2018-01-02 21:14:58 +0530

    Update check_for_palindromes.js

--- check_for_palindromes.js
+++ check_for_palindromes.js
@@ -1,12 +1,12 @@
 
 function palindrome(str) {
   // Good luck!
-  var rg =/[\W_]/g;
-  var low_rep=str.toLowerCase().replace(rg,'');
+  const rg =/[\W_]/g;
+  const low_rep=str.toLowerCase().replace(rg,'');
   //Converted to form in which no non-alphanumeric character is present i.e. string which           is to be compared
   
 
-  var final= low_rep.split('').reverse().join('');
+  const final= low_rep.split('').reverse().join('');
   //converted to palindrome form in which characters will be reversed and will be 
   //compared to non-alphanumeric string
   return final == low_rep;
