e4e86457b1b66207eaf1c2def97614eb365024fb
Meet Zaveri <meethpatel96@gmail.com>
2018-01-02 21:13:19 +0530

    Update chunky_monkey.js

--- chunky_monkey.js
+++ chunky_monkey.js
@@ -1,7 +1,7 @@
 function chunkArrayInGroups(arr, size) {
   // Break it up.
-  var group = [];
-  var position = 0;
+  const group = [];
+  const position = 0;
   
   while(position < arr.length) {
     group.push(arr.slice(position, position+=size));
